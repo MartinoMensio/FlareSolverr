1ad22ba8d9f21c449fb35dfed8e0367b5877bb09
Martino Mensio <martino.mensio@open.ac.uk>
2023-03-22 18:31:10 +0100

    lost during merge selector

--- src/flaresolverr_service.py
+++ src/flaresolverr_service.py
@@ -44,6 +44,8 @@ CHALLENGE_SELECTORS = [
     '#cf-challenge-running', '.ray_id', '.attack-box', '#cf-please-wait', '#challenge-spinner', '#trk_jschal_js',
     # Custom CloudFlare for EbookParadijs, Film-Paleis, MuziekFabriek and Puur-Hollands
     'td.info #js_info'
+    # Fairlane / pararius.com
+    'div.vc div.text-box h2'
 ]
 CAPTCHA_SELECTORS = captcha.solvers_by_css_selector.keys()
 SHORT_TIMEOUT = 10
